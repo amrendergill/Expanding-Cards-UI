a094226d70b2fde5090095bd5e5e75c595aeb27f
amrendergill <amrender028@gmail.com>
2021-08-27 18:23:41 +0530

    ini

---
+++ script.js
@@ -0,0 +1,13 @@
+const panels = document.querySelectorAll('.panel')
+panels.forEach((panel)=> {
+    panel.addEventListener('click',() => { 
+        removeActiveCLasses(); 
+        panel.classList.add('active')
+    })
+})
+function removeActiveCLasses(){
+    panels.forEach(panel =>{
+        panel.classList.remove('active')
+
+    })
+}
